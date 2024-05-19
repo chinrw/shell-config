35732d9e60f39fd2805bd162fd487f125ca81e94
Ruowen Qin <chinqrw@gmail.com>
2024-05-19 10:28:19 +0800

    Add init nix config
    Signed-off-by: Ruowen Qin <chinqrw@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "chin39-config";
+
+  inputs = {
+    # Nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    # Home manager
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , home-manager
+    , ...
+    } @ inputs:
+    let
+      inherit (self) outputs;
+      neovim-overlays = [
+        inputs.neovim-nightly-overlay.overlay
+      ];
+    in
+    {
+      # NixOS configuration entrypoint
+      # Available through 'nixos-rebuild --flake .#your-hostname'
+      nixosConfigurations = {
+        chin39-desktop = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs; };
+          # > Our main nixos configuration file <
+          modules = [ ./nixos/configuration.nix ];
+        };
+      };
+
+      # Standalone home-manager configuration entrypoint
+      # Available through 'home-manager --flake .#your-username@your-hostname'
+      homeConfigurations = {
+        # FIXME replace with your username@hostname
+        "chin39@chin39-desktop" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+          extraSpecialArgs = { inherit inputs outputs neovim-overlays; };
+          # > Our main home-manager configuration file <
+          modules = [ ./home-manager/home.nix ];
+        };
+      };
+    };
+}
---
+++ home-manager/home.nix
@@ -0,0 +1,82 @@
+# This is your home-manager configuration file
+# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
+{ inputs
+, lib
+, config
+, pkgs
+, neovim-overlays
+, ...
+}:
+let
+
+in
+{
+  # You can import other home-manager modules here
+  imports = [
+    # If you want to use home-manager modules from other flakes (such as nix-colors):
+    # inputs.nix-colors.homeManagerModule
+
+    # You can also split up your configuration and import pieces of it here:
+    # ./nvim.nix
+  ];
+
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # If you want to use overlays exported from other flakes:
+      # neovim-overlays
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+      # Workaround for https://github.com/nix-community/home-manager/issues/2942
+      allowUnfreePredicate = _: true;
+    };
+  };
+
+
+  home = {
+    # useGlobalPkgs = true;
+    sessionVariables = {
+      http_proxy = "http://10.0.0.242:10809";
+      https_proxy = "http://10.0.0.242:10809";
+    };
+    username = "chin39";
+    homeDirectory = "/home/chin39/";
+
+  };
+
+
+  # Add stuff for your user as you see fit:
+  # programs.neovim.enable = true;
+  home.packages = with pkgs; [
+    eza
+    jellyfin-media-player
+  ];
+
+  # Enable home-manager and git
+  programs.home-manager.enable = true;
+  programs.git.enable = true;
+
+  # Nicely reload system units when changing configs
+  # systemd.user.startServices = "sd-switch";
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "24.05";
+  # programs.bash = {
+  #   enable = true;
+  # };
+
+  # programs.zsh = {
+  #   enable = true;
+  # };
+
+}
