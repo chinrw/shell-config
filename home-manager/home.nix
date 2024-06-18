465c34401c65bfe98ab54775fef64cd19e929ea2
Ruowen Qin <chinqrw@gmail.com>
2024-06-18 12:58:20 +0800

    Add btop and tcpdump
    Signed-off-by: Ruowen Qin <chinqrw@gmail.com>

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -18,7 +18,7 @@ let
 
   proxyUrl =
     if (isWsl || isDesktop) then
-      # "http://10.0.0.242:10809"
+    # "http://10.0.0.242:10809"
       "http://192.168.0.101:7891"
     else if isWork then
       "http://squid.corp.redhat.com:3128"
@@ -110,7 +110,10 @@ in
         zellij
         delta
         duf # better df
-        tcpdump
+        tcpdump # monitor tcp
+
+        btop # system monitor
+        glances # same thing
 
         rclone
         gitoxide
