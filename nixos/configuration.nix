d73a29b4b789a22a58ebc24319b610774d28e856
Ruowen Qin <chinqrw@gmail.com>
2024-06-24 21:29:46 +0800

    Add samba service
    Signed-off-by: Ruowen Qin <chinqrw@gmail.com>

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -15,6 +15,7 @@
     # You can also split up your configuration and import pieces of it here:
     # ./users.nix
     ./networks
+    ./services/samba
 
     # Import your generated (nixos-generate-config) hardware configuration
     # ./hardware-configuration.nix
---
+++ nixos/services/samba/default.nix
@@ -0,0 +1,36 @@
+{ pkgs, ... }: {
+
+  services.samba = {
+    enable = true;
+    securityType = "user";
+    openFirewall = true;
+    extraConfig = ''
+      workgroup = WORKGROUP
+      server string = chin39smb
+      netbios name = chin39smb
+      security = user
+      #use sendfile = yes
+      server min protocol = SMB2_10
+      server max protocol = SMB3
+      #max protocol = smb2
+      # note: localhost is the ipv6 localhost ::1
+      hosts allow = 192.168.0. 127.0.0.1 10.0.0. 10.10.0. localhost
+      hosts deny = 0.0.0.0/0
+      # guest account = nobody
+      # map to guest = bad user
+    '';
+    shares = {
+      mounts = {
+        path = "/home/chin39/mounts";
+        browseable = "no";
+        writeable = "yes";
+        public = "no";
+        "valid users" = "chin39";
+        "guest ok" = "no";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+      };
+    };
+  };
+
+}
