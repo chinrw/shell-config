49c2228ce96fb15dfd1272cb2d7dd7e04833c5c5
Ruowen Qin <chinqrw@gmail.com>
2024-07-12 20:35:21 +0800

    Update nu config
    Signed-off-by: Ruowen Qin <chinqrw@gmail.com>

--- home-manager/programs/nushell/default.nix
+++ home-manager/programs/nushell/default.nix
@@ -1,16 +1,15 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [carapace];
+{ pkgs, ... }: {
+  home.packages = with pkgs; [ carapace ];
   programs.nushell = {
     enable = true;
     configFile.source = ./config.nu;
     envFile.source = ./env.nu;
-    # shellAliases = {
-    #   ll = "ls";
-    #   lt = "lsd --tree";
-    #   cat = "bat";
-    #   tree = "broot";
-    #   vim = "nvim";
-    # };
+    shellAliases = {
+      ll = "ls";
+      lt = "lsd --tree";
+      cat = "bat";
+      vim = "nvim";
+    };
     # extraConfig = ''
     #   source ~/repos/nu_scripts/modules/nix/nix.nu
     #   source ~/repos/nu_scripts/modules/git/git-v2.nu
