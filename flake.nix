96fabcdd57453fdb6472b111c24adc31097d1435
Ruowen Qin <chinqrw@gmail.com>
2024-08-11 10:43:22 +0800

    System update
    Signed-off-by: Ruowen Qin <chinqrw@gmail.com>

--- flake.nix
+++ flake.nix
@@ -88,10 +88,8 @@
 
       systems = [
         "aarch64-linux"
-        "i686-linux"
         "x86_64-linux"
         "aarch64-darwin"
-        "x86_64-darwin"
       ];
       # This is a function that generates an attribute by calling a function you
       # pass to it, with each system as an argument
