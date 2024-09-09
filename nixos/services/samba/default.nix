63f5ef33aacba0baf59c277df772afe227428b3d
Ruowen Qin <chinqrw@gmail.com>
2024-09-10 00:14:29 +0800

    Update samba config to fix new RFC
    Signed-off-by: Ruowen Qin <chinqrw@gmail.com>

--- nixos/services/samba/default.nix
+++ nixos/services/samba/default.nix
@@ -1,26 +1,27 @@
-{ pkgs, ... }: {
+{ ... }: {
 
   services.samba = {
     enable = true;
-    securityType = "user";
     openFirewall = true;
-    extraConfig = ''
-      workgroup = WORKGROUP
-      server string = chin39smb
-      netbios name = chin39smb
-      security = user
-      #use sendfile = yes
-      server min protocol = SMB2_10
-      server max protocol = SMB3
-      #max protocol = smb2
-      # note: localhost is the ipv6 localhost ::1
-      hosts allow = 192.168.0. 127.0.0.1 10.0.0. 10.10.0. localhost
-      hosts deny = 0.0.0.0/0
-      # guest account = nobody
-      # map to guest = bad user
-    '';
-    shares = {
-      mounts = {
+    # settings = {
+    # };
+    settings = {
+      "global" = {
+        "workgroup" = "WORKGROUP";
+        "server string" = "chin39smb";
+        "netbios name" = "chin39smb";
+        "security" = "user";
+        #use sendfile = yes
+        "server min protocol" = "SMB2_10";
+        "server max protocol" = "SMB3";
+        #max protocol = smb2
+        # note: localhost is the ipv6 localhost ::1
+        "hosts allow" = "192.168.0., 127.0.0.1, 10.0.0., 10.10.0., localhost";
+        "hosts deny" = "0.0.0.0/0";
+        # guest account = nobody
+        # map to guest = bad user
+      };
+      "mounts" = {
         path = "/home/chin39/mounts";
         browseable = "yes";
         writeable = "yes";
