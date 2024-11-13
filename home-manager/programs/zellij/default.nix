f409708a4934f1cf7cc983075d845a1e5d2258e4
Ruowen Qin <chinqrw@gmail.com>
2024-11-13 23:29:48 +0800

    Use zjstatus from pkgs
    Signed-off-by: Ruowen Qin <chinqrw@gmail.com>

--- home-manager/programs/zellij/default.nix
+++ home-manager/programs/zellij/default.nix
@@ -1,8 +1,13 @@
 { lib, pkgs, config, ... }: {
 
-  home.file."${config.xdg.configHome}/zellij" = {
-    source = ../../../zellij;
-    recursive = true;
+  home.file = {
+    "${config.xdg.configHome}/zellij" = {
+      source = ../../../zellij;
+      recursive = true;
+    };
+    "${config.xdg.configHome}/zellij-plugins/zjstatus.wasm" = {
+      source = "${pkgs.zjstatus}/bin/zjstatus.wasm";
+    };
   };
   # programs.zellij = {
   #
