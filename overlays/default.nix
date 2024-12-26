6652ed4d9c47672efc67eac8f2ca2edc59022bc5
Ruowen Qin <chinqrw@gmail.com>
2024-12-26 10:15:09 +0800

    fix awscli2 and zsh-fzf-tab compile issue
    Signed-off-by: Ruowen Qin <chinqrw@gmail.com>

--- overlays/default.nix
+++ overlays/default.nix
@@ -19,10 +19,42 @@
     #   # We can change the version of the package
     #   extraRustcOpts = "-C target-cpu=native -C link-arg=-fuse-ld=mold -Clinker-plugin-lto";
     # });
-    # zellij = prev.zellij.overrideAttrs (old: {
+    # zellij = prev.zellij.overrideAttrs (old: rec {
     #   # We can change the version of the package
     #   extraRustcOpts = "-C target-cpu=native -C link-arg=-fuse-ld=mold -Clinker-plugin-lto";
     # });
+
+    awscli2 = prev.awscli2.overrideAttrs (old: {
+      # We can change the version of the package
+      postPatch = ''
+        substituteInPlace pyproject.toml \
+          --replace-fail 'flit_core>=3.7.1,<3.9.1' 'flit_core>=3.7.1' \
+          --replace-fail 'awscrt>=0.19.18,<=0.22.0' 'awscrt>=0.22.0' \
+          --replace-fail 'cryptography>=40.0.0,<43.0.2' 'cryptography>=43.0.0' \
+          --replace-fail 'distro>=1.5.0,<1.9.0' 'distro>=1.5.0' \
+          --replace-fail 'docutils>=0.10,<0.20' 'docutils>=0.10' \
+          --replace-fail 'prompt-toolkit>=3.0.24,<3.0.39' 'prompt-toolkit>=3.0.24'
+
+        substituteInPlace requirements-base.txt \
+          --replace-fail "wheel==0.43.0" "wheel>=0.43.0"
+
+        # Upstream needs pip to build and install dependencies and validates this
+        # with a configure script, but we don't as we provide all of the packages
+        # through PYTHONPATH
+        sed -i '/pip>=/d' requirements/bootstrap.txt
+      '';
+    });
+
+    zsh-fzf-tab = prev.zsh-fzf-tab.override (old: {
+      stdenv = final.clangStdenv;
+    });
+
+
+    buildRustPackage = prev.buildRustPackage.overrideAttrs (old: {
+      postBuild = old.postBuild or "" + ''
+        export RUSTFLAGS="$RUSTFLAGS -C target-cpu=native -C link-arg=-fuse-ld=mold"
+      '';
+    });
   };
 
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
