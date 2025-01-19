b0ae5f07aa293fe0d8778808249ca2df26bacf86
Ruowen Qin <chinqrw@gmail.com>
2025-01-19 18:01:42 +0800

    Enable nvidia driver
    Signed-off-by: Ruowen Qin <chinqrw@gmail.com>

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -5,7 +5,32 @@
 , config
 , pkgs
 , ...
-}: {
+}:
+let
+  wsl-lib = pkgs.runCommand "wsl-lib" { } ''
+    mkdir -p "$out/lib"
+    # # We can't just symlink the lib directory, because it will break merging with other drivers that provide the same directory
+    ln -s /usr/lib/wsl/lib/libcudadebugger.so.1 "$out/lib"
+    ln -s /usr/lib/wsl/lib/libcuda.so "$out/lib"
+    ln -s /usr/lib/wsl/lib/libcuda.so.1 "$out/lib"
+    ln -s /usr/lib/wsl/lib/libcuda.so.1.1 "$out/lib"
+    ln -s /usr/lib/wsl/lib/libd3d12core.so "$out/lib"
+    ln -s /usr/lib/wsl/lib/libd3d12.so "$out/lib"
+    ln -s /usr/lib/wsl/lib/libdxcore.so "$out/lib"
+    ln -s /usr/lib/wsl/lib/libnvcuvid.so "$out/lib"
+    ln -s /usr/lib/wsl/lib/libnvcuvid.so.1 "$out/lib"
+    ln -s /usr/lib/wsl/lib/libnvdxdlkernels.so "$out/lib"
+    ln -s /usr/lib/wsl/lib/libnvidia-encode.so "$out/lib"
+    ln -s /usr/lib/wsl/lib/libnvidia-encode.so.1 "$out/lib"
+    ln -s /usr/lib/wsl/lib/libnvidia-ml.so.1 "$out/lib"
+    ln -s /usr/lib/wsl/lib/libnvidia-opticalflow.so "$out/lib"
+    ln -s /usr/lib/wsl/lib/libnvidia-opticalflow.so.1 "$out/lib"
+    ln -s /usr/lib/wsl/lib/libnvoptix.so.1 "$out/lib"
+    ln -s /usr/lib/wsl/lib/libnvwgf2umx.so "$out/lib"
+    ln -s /usr/lib/wsl/lib/nvidia-smi "$out/lib"
+  '';
+in
+{
   # You can import other NixOS modules here
   imports = [
     # If you want to use modules from other flakes (such as nixos-hardware):
@@ -121,14 +146,14 @@
     };
   };
   hardware = {
+    nvidia-container-toolkit = {
+      enable = true;
+      mount-nvidia-executables = true;
+    };
     nvidia = {
       modesetting.enable = true;
       nvidiaSettings = false;
       open = false;
-    };
-    nvidia-container-toolkit = {
-      enable = true;
-      mount-nvidia-executables = false;
     };
   };
   services.xserver.videoDrivers = [ "nvidia" ];
@@ -186,6 +211,7 @@
     nix-ld = {
       enable = true;
       package = pkgs.nix-ld-rs;
+      libraries = [ wsl-lib ];
     };
 
     zsh.enable = true;
