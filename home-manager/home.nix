fff2e74f6680131889cbaf8f1040a7f3330665af
Ruowen Qin <chinqrw@gmail.com>
2025-01-25 15:14:55 +0800

    remove unused awscli2
    Signed-off-by: Ruowen Qin <chinqrw@gmail.com>

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -209,7 +209,7 @@ in
     enable = true;
     # the specified packages as well as 1Password CLI will be
     # automatically installed and configured to use shell plugins
-    plugins = with pkgs; [ gh awscli2 ];
+    plugins = with pkgs; [ gh ];
   };
 
 
