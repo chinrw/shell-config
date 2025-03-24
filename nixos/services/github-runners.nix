e47f98b755bef9995546f8b8d626ecbb648d1387
Ruowen Qin <chinqrw@gmail.com>
2025-03-24 23:40:08 +0800

    Update github runner config
    Signed-off-by: Ruowen Qin <chinqrw@gmail.com>

--- nixos/services/github-runners.nix
+++ nixos/services/github-runners.nix
@@ -1,5 +1,17 @@
 { config, pkgs, ... }: {
 
+  users.groups.github-runners = { };
+
+  # Define the GitHub Actions runner service user
+  users.users.midashood = {
+    isSystemUser = true;
+    description = "GitHub Actions Runner Service User";
+    createHome = false;
+    group = "github-runners";
+    extraGroups = [ "kvm" ];
+    shell = "/run/current-system/sw/bin/nologin";
+  };
+
   services.github-runners = {
     runner1 = {
       enable = true;
@@ -7,6 +19,10 @@
       tokenFile = config.sops.secrets."github-runners/midashood".path;
       url = "https://github.com/rex-rs/rex";
       extraLabels = [ "nix" ];
+      user = "midashood";
+      extraEnvironment = {
+        all_proxy = "http://10.0.0.242:10809";
+      };
     };
   };
 }
