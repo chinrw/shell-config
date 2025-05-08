4ae3dc6fd1f8da80909263648f1bec712e36b25e
Ruowen Qin <chinqrw@gmail.com>
2025-05-08 11:11:23 +0800

    Disable ollama on vm
    Signed-off-by: Ruowen Qin <chinqrw@gmail.com>

--- nixos/services/llm.nix
+++ nixos/services/llm.nix
@@ -1,24 +1,25 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, ... }:
+{
 
   services = {
-    ollama = {
-      enable = true;
-      package = pkgs.ollama;
-      acceleration = "cuda";
-      host = "0.0.0.0";
-      environmentVariables = {
-        http_proxy = "http://192.168.0.101:7891";
-        https_proxy = "http://192.168.0.101:7891";
-        LD_LIBRARY_PATH = "\${LD_LIBRARY_PATH}:/usr/lib/wsl/lib";
-      };
-    };
+    # ollama = {
+    #   enable = true;
+    #   package = pkgs.ollama;
+    #   acceleration = "cuda";
+    #   host = "0.0.0.0";
+    #   environmentVariables = {
+    #     http_proxy = "http://192.168.0.101:7891";
+    #     https_proxy = "http://192.168.0.101:7891";
+    #     LD_LIBRARY_PATH = "\${LD_LIBRARY_PATH}:/usr/lib/wsl/lib";
+    #   };
+    # };
     open-webui = {
       enable = true;
       package = pkgs.unstable.open-webui;
       host = "192.168.0.201";
       environment = {
-        http_proxy = "http://192.168.0.101:7891";
-        https_proxy = "http://192.168.0.101:7891";
+        http_proxy = "http://192.168.0.201:10809";
+        https_proxy = "http://192.168.0.201:10809";
       };
     };
     # llama-cpp = {
