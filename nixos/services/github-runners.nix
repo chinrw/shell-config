441fb2a327532c9bd9fada3af41e0fa5def45f6a
Ruowen Qin <chinqrw@gmail.com>
2025-05-11 20:09:20 +0800

    Add github runner
    Signed-off-by: Ruowen Qin <chinqrw@gmail.com>

--- nixos/services/github-runners.nix
+++ nixos/services/github-runners.nix
@@ -1,5 +1,49 @@
-{ config, pkgs, ... }:
 {
+  config,
+  lib,
+  pkgs,
+  hostname,
+  ...
+}:
+let
+  runnersByHost = {
+    "wsl-mini" = {
+      name = "midashood";
+      tokenSecret = config.sops.secrets."github-runners/midashood".path;
+      proxy = "http://10.0.0.242:10809";
+    };
+    "vm-nix" = {
+      name = "Constantinople";
+      tokenSecret = config.sops.secrets."github-runners/Constantinople".path;
+      proxy = "http://127.0.0.1:10809";
+    };
+  };
+
+  thisHostCfg = runnersByHost.${hostname} or null;
+
+in
+{
+  assertions = [
+    {
+      assertion = thisHostCfg ? name;
+      message = "✗ No GitHub-runner token configured for host “${hostname}”.";
+    }
+  ];
+
+  services.github-runners = {
+    runner1 = {
+      enable = true;
+      name = thisHostCfg.name;
+      tokenFile = config.sops.secrets."github-runners/${thisHostCfg.name}".path;
+      url = "https://github.com/rex-rs/rex";
+      extraLabels = [ "nix" ];
+      user = "midashood";
+      replace = true;
+      extraEnvironment = lib.mkIf (thisHostCfg ? proxy) {
+        all_proxy = thisHostCfg.proxy;
+      };
+    };
+  };
 
   users.groups.github-runners = { };
 
@@ -11,21 +55,5 @@
     group = "github-runners";
     extraGroups = [ "kvm" ];
     shell = "/run/current-system/sw/bin/nologin";
-  };
-
-  services.github-runners = {
-    runner1 = {
-      enable = true;
-      name = "midashood";
-      tokenFile = config.sops.secrets."github-runners/midashood".path;
-      url = "https://github.com/rex-rs/rex";
-      extraLabels = [ "nix" ];
-      user = "midashood";
-      workDir = "/var/lib/github-runner/midashood";
-      replace = true;
-      extraEnvironment = {
-        all_proxy = "http://10.0.0.242:10809";
-      };
-    };
   };
 }
