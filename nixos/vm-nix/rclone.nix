b4983f8e8e4cbb8e2f8dab9ca8e2e99c45b19a69
Ruowen Qin <chinqrw@gmail.com>
2025-07-13 21:25:58 +0800

    Update config
    Signed-off-by: Ruowen Qin <chinqrw@gmail.com>

--- nixos/vm-nix/rclone.nix
+++ nixos/vm-nix/rclone.nix
@@ -3,18 +3,29 @@
   systemd.services."rclone_downloader" = {
     description = "rclone baidu netdisk";
     serviceConfig = {
-      Type = "oneshot";
+      Type = "simple";
       User = "chin39";
-      ExecStart = "${pkgs.rclone}/bin/rclone move -v baidu:baidu/apps/Alist/ /mnt/data/baidu \
-                    --transfers 4 --multi-thread-streams 0 --timeout 0 \
-                    --multi-thread-chunk-size 32M --delete-empty-src-dirs";
+      ExecStart = ''
+        ${pkgs.rclone}/bin/rclone move \
+                    baidu:baidu/apps/Alist/ \
+                    /mnt/data/baidu \
+                    --log-systemd \
+                    --stats-one-line \
+                    --log-level INFO \
+                    --transfers 4 \
+                    --multi-thread-streams 0 \
+                    --timeout 0 \
+                    --multi-thread-chunk-size 32M \
+                    --delete-empty-src-dirs \
+                    --bwlimit 35M
+      '';
     };
 
     # If the command needs extra packages on $PATH
     # path = [ pkgs.curl ];
   };
 
-  systemd.timers."rclone_download" = {
+  systemd.timers."rclone_downloader" = {
     wantedBy = [ "timers.target" ]; # auto-start at boot
     timerConfig = {
       # First run 5 min after boot, then every 15 min
