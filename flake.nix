20cbefb31c45bb77d493f890d39d83e937cb191d
Ruowen Qin <chinqrw@gmail.com>
2025-12-01 12:59:38 +0800

    Revert "lock home-manager to release-25.11"
    This reverts commit 1f7c53723dd8c4b50cca351c029185da98f3292e.

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
 
     # Home manager
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
