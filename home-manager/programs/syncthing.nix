44ae6f9f2b863ad0a010858b483177c649da5534
Ruowen Qin <chinqrw@gmail.com>
2026-04-13 19:27:09 +0800

    add overrideFolders config for manual edit
    Signed-off-by: Ruowen Qin <chinqrw@gmail.com>

--- home-manager/programs/syncthing.nix
+++ home-manager/programs/syncthing.nix
@@ -19,23 +19,29 @@ in
   services.syncthing = lib.mkIf isEnabled {
     enable = true;
     guiAddress = "0.0.0.0:8384";
+    overrideFolders = false;
 
     guiCredentials = {
       username = "chin39";
       passwordFile = config.sops.secrets."syncthing-gui".path;
     };
 
-    settings = lib.mkIf (hostname == "proxmox") {
-      devices.windows-desktop = {
-        id = "XTINWEA-LVW3WH3-4L5P67N-S3NOKFI-I6LGIW7-SSDXS6Z-R67CYZU-NBJVAQ6";
-        addresses = [ "tcp://192.168.0.101:22000" ];
-        allowedNetworks = [ "192.168.0.0/24" ];
-      };
-      folders.onedrive = {
-        path = "/mnt/elysion/data/Documents/onedrive";
-        devices = [ "windows-desktop" ];
-        type = "sendreceive";
-      };
-    };
+    settings = lib.mkMerge [
+      {
+        options.urAccepted = -1;
+      }
+      (lib.mkIf (hostname == "proxmox") {
+        devices.windows-desktop = {
+          id = "XTINWEA-LVW3WH3-4L5P67N-S3NOKFI-I6LGIW7-SSDXS6Z-R67CYZU-NBJVAQ6";
+          addresses = [ "tcp://192.168.0.101:22000" ];
+          allowedNetworks = [ "192.168.0.0/24" ];
+        };
+        folders.onedrive = {
+          path = "/mnt/elysion/data/Documents/onedrive";
+          devices = [ "windows-desktop" ];
+          type = "sendreceive";
+        };
+      })
+    ];
   };
 }
